bca8b69cbc2fa3784533d24c492719b294d8673f
rajan1220 <rajan.gcd@gmail.com>
2015-02-13 12:17:43 +0530

    php start

---
+++ php/home.php
@@ -0,0 +1,15 @@
+<html>
+	<head>
+		<title>php</title>
+	</head>
+	<body>
+		<?php	
+			function test()
+			{
+				$x="This is the first page !";
+				echo "$x" ;
+			}
+			test();
+		?>
+	</body>
+</html>
