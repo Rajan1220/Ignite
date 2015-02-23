5519ff015659fbb0fcf8792190de7afe83401312
rajan1220 <rajan.gcd@gmail.com>
2015-02-23 14:37:26 +0530

    profile page in php

---
+++ php/profile.php
@@ -0,0 +1,13 @@
+<html>
+	<head>
+		<title>Profile</title>
+	</head>
+	<body>
+	<?php
+	name=$_post["user_name"];	
+	$email=$_post["emailid"];
+	echo "Name:"."<input name="$name">";
+	echo "Email:"."<input name="$email" >";
+	?>
+	</body>
+<html>
